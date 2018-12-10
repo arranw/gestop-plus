5250784c99fcbe036ba863e20284e7fdb227c781
arranw <arranwoodruff@gmail.com>
2018-12-09 17:43:13 -0700

    Update tm-plus.js

--- tm-plus.js
+++ tm-plus.js
@@ -5,26 +5,66 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://home.aeromag2000.com/gestoplight/
-// @grant        none
-// @require http://code.jquery.com/jquery-1.8.24.min.js
+// @grant        GM_addStyle
+// @require      http://code.jquery.com/jquery-1.8.24.min.js
+// @require      Common.js
+// @require      DeicingVM.js
+// @require      knockout-3.4.2.js
+// @updateURL    https://raw.githubusercontent.com/arranw/gestop-plus/master/tm-plus.js
 // ==/UserScript==
-// update test
+
 var $ = window.jQuery;
+// // remove aeromag logo until i make a darkened version
+// var $image = $("img[]");
+// $image.removeAttr('src');
+// $("style=\"background-color:#fff;\"").style = "style='background-color:#000;'";
+
 
 var flightBox = document.getElementById('dvFlightStrip');
 
 var flightCodeBox = document.getElementById('txtFlightNo');
 flightCodeBox.classList.remove("setCarrier");
-var t1Time = document.getElementById("DeicingTimes_Type1FinishTimeStr");
-t1Time.classList.remove("time");
-t1Time.classList.add("timetest");
+
+// time boxes
+// var t1Start = document.getElementById("DeicingTimes_Type1StartTimeStr");
+// t1Start.classList.remove("time");
+// t1Start.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
+// t1Start.onclick = currentTime;
+// var t1Stop = document.getElementById("DeicingTimes_Type1FinishTimeStr");
+// t1Stop.classList.remove("time");
+// t1Stop.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
+// var t4Start = document.getElementById("DeicingTimes_Type4StartTimeStr");
+// t4Start.classList.remove("time");
+// t4Start.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
+// var t4Stop = document.getElementById("DeicingTimes_Type4FinishTimeStr");
+// t4Stop.classList.remove("time");
+// t4Stop.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
+// time boxes
+
+
+
+function currentTime(input) {
+    var box = input;
+
+
+    box.value = "tesT";
+}
 
 var array = ["","WJA","WEN","PCO","QXE","TSC"];
+
+var br = document.createElement("br");
+// var airlineLabel = document.createElement("label");
+// airlineLabel.style = "font-size: 0.9em";
+// airlineLabel.innerHTML = "Airline";
+
 
 var selectList = document.createElement("select");
 selectList.id = "mySelect";
 selectList.style.width = "150px";
-flightBox.parentElement.insertBefore(selectList, flightBox);
+findFirstDescendant("frmFlightStrip", "th").appendChild(br);
+// findFirstDescendant("frmFlightStrip", "th", 0).appendChild(airlineLabel);
+findFirstDescendant("frmFlightStrip", "th").appendChild(selectList);
+// findFirstDescendant("frmFlightStrip", "th", 0).insertBefore(selectList, null);
 
 for (var i = 0; i < array.length; i++) {
     var option = document.createElement("option");
@@ -38,14 +78,59 @@ addAlCode.type="button";
 addAlCode.value="Go";
 addAlCode.onclick=alCode;
 addAlCode.style.marginLeft="10px";
-flightBox.parentElement.insertBefore(addAlCode, flightBox);
+findFirstDescendant("frmFlightStrip", "th").appendChild(addAlCode);
+
 
 var addAcType = document.createElement("input");
 addAcType.type="button";
 addAcType.value="AC";
 addAcType.onclick=addAc;
 addAcType.style.marginLeft="10px";
-flightBox.parentElement.insertBefore(addAcType, flightBox);
+findFirstDescendant("frmFlightStrip", "th").appendChild(addAcType);
+
+var themeTicker = document.createElement("input");
+themeTicker.type = "checkbox";
+themeTicker.checked = false;
+// changeTheme();
+themeTicker.onclick = changeTheme;
+document.body.appendChild(themeTicker);
+
+function changeTheme() {
+    var bgBlack = "#000";
+    var themeBg;
+    var themeFg;
+    if (themeTicker.checked) {
+        themeBg = "{background-color: #000 !important}";
+        themeFg = "{color: #33BBFF !important}";
+        findFirstDescendant("body", "table").classList.remove("border1");
+        findFirstDescendant("ui-id-2", "td").classList.remove("border1");
+
+//         findFirstDescendant("dvFlightStrip", "table").classList.remove("border1");
+        document.getElementById("tdFlightsInColdStorage").classList.remove("border1");
+        document.getElementById("tdFlightsInProgress").classList.remove("border1");
+        document.getElementById("tdFlightsInProgress").classList.add("border2");
+        GM_addStyle("body, tr, th, tbody {border: 0px solid #fff !important}");
+        GM_addStyle(".ui-state-default {background: #000 !important}");
+        GM_addStyle(".ui-state-default {color: #ffffff !important}");
+        GM_addStyle("fieldset {border: 0px !important}");
+    } else {
+        themeBg = "{background-color: #fff !important}";
+        themeFg = "{color: #000 !important}";
+        document.getElementById("tdFlightsInColdStorage").classList.add("border1");
+        document.getElementById("tdFlightsInProgress").classList.remove("border2");
+        document.getElementById("tdFlightsInProgress").classList.add("border1");
+        GM_addStyle("body, tr, th, tbody {border: 0px solid #fff !important}");
+        GM_addStyle(".ui-state-default {background: #9399a0 !important}");
+        GM_addStyle(".ui-state-default {color: #e0e0e0 !important}");
+    }
+    GM_addStyle(".border2 {padding: 5px!important}");
+    GM_addStyle(".border2 {margin-bottom: 5px!important}");
+    GM_addStyle(".border2 {overflow: auto!important}");
+
+
+    GM_addStyle("body, html, footer, div, th, tr, td, section, select, input, textarea, li " + themeBg); // active flights
+    GM_addStyle("body, html, footer, div, th, tr, td, select, input, textarea " + themeFg);
+}
 
 function addAc() {
     var acBox = document.getElementById("txtAircraftTypeCd");
@@ -101,4 +186,13 @@ function alCode() {
     }
 
     oaciDD.value = select.value;
+}
+
+function findFirstDescendant(parent, tagname) {
+   parent = document.getElementById(parent);
+   var descendants = parent.getElementsByTagName(tagname);
+   if ( descendants.length ){
+      return descendants[0];
+   }
+   return null;
 }
