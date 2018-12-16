25df84b666de0bc784f481cbf699c905223e338d
arranw <arranwoodruff@gmail.com>
2018-12-16 07:19:46 -0700

    Update and rename tm-plus.js to Gs-Plus.js

--- Gs-Plus.js
+++ Gs-Plus.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Live
+// @name         Gs Plus
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -14,6 +14,7 @@
 // ==/UserScript==
 
 var $ = window.jQuery;
+
 // // remove aeromag logo until i make a darkened version
 // var $image = $("img[]");
 // $image.removeAttr('src');
@@ -26,45 +27,41 @@ var flightCodeBox = document.getElementById('txtFlightNo');
 flightCodeBox.classList.remove("setCarrier");
 
 // time boxes
-// var t1Start = document.getElementById("DeicingTimes_Type1StartTimeStr");
-// t1Start.classList.remove("time");
-// t1Start.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
-// t1Start.onclick = currentTime;
-// var t1Stop = document.getElementById("DeicingTimes_Type1FinishTimeStr");
-// t1Stop.classList.remove("time");
-// t1Stop.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
-// var t4Start = document.getElementById("DeicingTimes_Type4StartTimeStr");
-// t4Start.classList.remove("time");
-// t4Start.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
-// var t4Stop = document.getElementById("DeicingTimes_Type4FinishTimeStr");
-// t4Stop.classList.remove("time");
-// t4Stop.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
-// time boxes
 
-
-
-function currentTime(input) {
-    var box = input;
-
-
-    box.value = "tesT";
+    var t1Start = document.getElementById("DeicingTimes_Type1StartTimeStr");
+    t1Start.classList.remove("time");
+    t1Start.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
+    var t1Stop = document.getElementById("DeicingTimes_Type1FinishTimeStr");
+    t1Stop.classList.remove("time");
+    t1Stop.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
+    var t4Start = document.getElementById("DeicingTimes_Type4StartTimeStr");
+    t4Start.classList.remove("time");
+    t4Start.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
+    var t4Stop = document.getElementById("DeicingTimes_Type4FinishTimeStr");
+    t4Stop.classList.remove("time");
+    t4Stop.style = "width: 67px;margin: 0px;padding: 0px; color: #000 !important";
 }
+// time boxes
 
 var array = ["","WJA","WEN","PCO","QXE","TSC"];
 
 var br = document.createElement("br");
-// var airlineLabel = document.createElement("label");
-// airlineLabel.style = "font-size: 0.9em";
-// airlineLabel.innerHTML = "Airline";
+findFirstDescendant("frmFlightStrip", "th").remove();
+findFirstDescendant("frmFlightStrip", "table").classList.remove("border1");
 
+var flightStrip = document.getElementById('frmFlightStrip');
+var fsWrapper = document.createElement("fieldset");
+fsWrapper.classList.add("border1");
+fsWrapper.id="fsWrapper";
+fsWrapper.innerHTML = "<legend style=\"font-size:16px; font-weight:bold;\">Flight Strip</legend>";
+flightStrip.parentNode.insertBefore(fsWrapper, flightStrip);
+fsWrapper.appendChild(flightStrip);
 
 var selectList = document.createElement("select");
 selectList.id = "mySelect";
+selectList.style.marginLeft = "2px";
 selectList.style.width = "150px";
-findFirstDescendant("frmFlightStrip", "th").appendChild(br);
-// findFirstDescendant("frmFlightStrip", "th", 0).appendChild(airlineLabel);
-findFirstDescendant("frmFlightStrip", "th").appendChild(selectList);
-// findFirstDescendant("frmFlightStrip", "th", 0).insertBefore(selectList, null);
+flightStrip.insertBefore(selectList, findFirstDescendant("frmFlightStrip", "table"));
 
 for (var i = 0; i < array.length; i++) {
     var option = document.createElement("option");
@@ -78,59 +75,14 @@ addAlCode.type="button";
 addAlCode.value="Go";
 addAlCode.onclick=alCode;
 addAlCode.style.marginLeft="10px";
-findFirstDescendant("frmFlightStrip", "th").appendChild(addAlCode);
-
+flightStrip.insertBefore(addAlCode, findFirstDescendant("frmFlightStrip", "table"));
 
 var addAcType = document.createElement("input");
 addAcType.type="button";
 addAcType.value="AC";
 addAcType.onclick=addAc;
 addAcType.style.marginLeft="10px";
-findFirstDescendant("frmFlightStrip", "th").appendChild(addAcType);
-
-var themeTicker = document.createElement("input");
-themeTicker.type = "checkbox";
-themeTicker.checked = false;
-// changeTheme();
-themeTicker.onclick = changeTheme;
-document.body.appendChild(themeTicker);
-
-function changeTheme() {
-    var bgBlack = "#000";
-    var themeBg;
-    var themeFg;
-    if (themeTicker.checked) {
-        themeBg = "{background-color: #000 !important}";
-        themeFg = "{color: #33BBFF !important}";
-        findFirstDescendant("body", "table").classList.remove("border1");
-        findFirstDescendant("ui-id-2", "td").classList.remove("border1");
-
-//         findFirstDescendant("dvFlightStrip", "table").classList.remove("border1");
-        document.getElementById("tdFlightsInColdStorage").classList.remove("border1");
-        document.getElementById("tdFlightsInProgress").classList.remove("border1");
-        document.getElementById("tdFlightsInProgress").classList.add("border2");
-        GM_addStyle("body, tr, th, tbody {border: 0px solid #fff !important}");
-        GM_addStyle(".ui-state-default {background: #000 !important}");
-        GM_addStyle(".ui-state-default {color: #ffffff !important}");
-        GM_addStyle("fieldset {border: 0px !important}");
-    } else {
-        themeBg = "{background-color: #fff !important}";
-        themeFg = "{color: #000 !important}";
-        document.getElementById("tdFlightsInColdStorage").classList.add("border1");
-        document.getElementById("tdFlightsInProgress").classList.remove("border2");
-        document.getElementById("tdFlightsInProgress").classList.add("border1");
-        GM_addStyle("body, tr, th, tbody {border: 0px solid #fff !important}");
-        GM_addStyle(".ui-state-default {background: #9399a0 !important}");
-        GM_addStyle(".ui-state-default {color: #e0e0e0 !important}");
-    }
-    GM_addStyle(".border2 {padding: 5px!important}");
-    GM_addStyle(".border2 {margin-bottom: 5px!important}");
-    GM_addStyle(".border2 {overflow: auto!important}");
-
-
-    GM_addStyle("body, html, footer, div, th, tr, td, section, select, input, textarea, li " + themeBg); // active flights
-    GM_addStyle("body, html, footer, div, th, tr, td, select, input, textarea " + themeFg);
-}
+flightStrip.insertBefore(addAcType, findFirstDescendant("frmFlightStrip", "table"));
 
 function addAc() {
     var acBox = document.getElementById("txtAircraftTypeCd");
@@ -193,6 +145,15 @@ function findFirstDescendant(parent, tagname) {
    var descendants = parent.getElementsByTagName(tagname);
    if ( descendants.length ){
       return descendants[0];
+   }
+   return null;
+}
+
+function findDescendant(parent, tagname, n) {
+   parent = document.getElementById(parent);
+   var descendants = parent.getElementsByTagName(tagname);
+   if ( descendants.length ){
+      return descendants[n];
    }
    return null;
 }
