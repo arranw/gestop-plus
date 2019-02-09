426b017fb9e6a3959b0a359f938c65b8ea18fd26
arranw <arranwoodruff@gmail.com>
2019-02-08 17:31:12 -0700

    bugfixes

--- gsplus.js
+++ gsplus.js
@@ -24,7 +24,7 @@
     var flightBox = document.getElementById('dvFlightStrip');
 
     var addedComponents = document.createElement("div");
-//     addedComponents.style.width = "40%";
+    //     addedComponents.style.width = "40%";
     addedComponents.id = "addedComps";
 
     var inputBase = document.createElement("input");
@@ -147,9 +147,7 @@
         var select = document.getElementById('mySelect');
         var regBox = document.getElementById('txtRegistration');
         var flightBox = document.getElementById('txtFlightNo');
-
-        flightBox.value = flightCode.value;
-        regBox.value = tailCode.value;
+        var acBox = document.getElementById("txtAircraftTypeCd");
 
         var hasFlightCode = false;
         var hasRegCode = false;
@@ -157,225 +155,232 @@
         var flightTextValue = flightCodeBox.value.toUpperCase();
         var regTextValue = regBox.value.toUpperCase();
 
-        // begin loop at 1 to avoid searching for "" in array
-        for (var i = 1; i < array.length; i++) {
-            if (flightTextValue.includes(array[i])) {
-                hasFlightCode = true;
-            }
-            if (regTextValue.includes(array[i])) {
-                hasRegCode = true;
-            }
-        }
+        
 
-        if (hasFlightCode) {
-            flightCodeBox.value = select.value + flightCodeBox.value.substr(3, flightCodeBox.value.length);
-        } else {
-            flightCodeBox.value = select.value + flightCodeBox.value;
-        }
-        if (hasRegCode) {
-            regBox.value = select.value + regBox.value.substr(3, regBox.value.length);
-        } else {
-            regBox.value = select.value + regBox.value;
-        }
 
-        var acBox = document.getElementById("txtAircraftTypeCd");
-        regBox = document.getElementById('txtRegistration');
+        if (select.value !== "") {
+            flightBox.value = flightCode.value;
+            regBox.value = tailCode.value;
 
-        for (var j =0; j < 999; j++) {
-            if (j == parseInt(regBox.value.substr(3, regBox.value.length))) {
-                //                 alert("nonono");
-                if (j >= 0 && j <= 15) {
-                    acBox.value = "B73W";
-                } else if (j >= 201 && j <= 266) {
-                    acBox.value = "B73W";
-                } else if (j >= 300 && j <= 399) {
-                    acBox.value = "B38M";
-                } else if (j >= 400 && j <= 460) {
-                    acBox.value = "DH8D";
-                } else if (j >= 575 && j <= 589) {
-                    acBox.value = "SF34";
-                } else if (j >= 601 && j <= 615) {
-                    acBox.value = "B736";
-                } else if (j >= 670 && j <= 673) {
-                    acBox.value = "B76W";
-                } else if (j >= 801 && j <= 853) {
-                    acBox.value = "B73H";
-                } else {
-
+            // begin loop at 1 to avoid searching for "" in array
+            for (var i = 1; i < array.length; i++) {
+                if (flightTextValue.includes(array[i])) {
+                    hasFlightCode = true;
+                }
+                if (regTextValue.includes(array[i])) {
+                    hasRegCode = true;
                 }
             }
-            if (regBox.value.includes("GTXO")) acBox.value = "A321";
+
+            if (hasFlightCode) {
+                flightCodeBox.value = select.value + flightCodeBox.value.substr(3, flightCodeBox.value.length);
+            } else {
+                flightCodeBox.value = select.value + flightCodeBox.value;
+            }
+            if (hasRegCode) {
+                regBox.value = select.value + regBox.value.substr(3, regBox.value.length);
+            } else {
+                regBox.value = select.value + regBox.value;
+            }
+
+            for (var j =0; j < 999; j++) {
+                if (j == parseInt(regBox.value.substr(3, regBox.value.length))) {
+                    //                 alert("nonono");
+                    if (j >= 0 && j <= 15) {
+                        acBox.value = "B73W";
+                    } else if (j >= 201 && j <= 266) {
+                        acBox.value = "B73W";
+                    } else if (j >= 300 && j <= 399) {
+                        acBox.value = "B38M";
+                    } else if (j >= 400 && j <= 460) {
+                        acBox.value = "DH8D";
+                    } else if (j >= 575 && j <= 589) {
+                        acBox.value = "SF34";
+                    } else if (j >= 601 && j <= 615) {
+                        acBox.value = "B736";
+                    } else if (j >= 670 && j <= 673) {
+                        acBox.value = "B76W";
+                    } else if (j >= 801 && j <= 853) {
+                        acBox.value = "B73H";
+                    } else {
+
+                    }
+                }
+                if (regBox.value.includes("GTXO")) acBox.value = "A321";
+            }
+
+
+
+            // Gate Selector
+            var gateSelect = document.getElementById("Gate_Id");
+
+            if (gateCode.value == "A2" || gateCode.value == "a2") {
+                gateSelect.value = "12778";}
+            else if (gateCode.value == "A9" || gateCode.value == "a9") {
+                gateSelect.value = "12930";}
+            else if (gateCode.value == "1") {
+                gateSelect.value = 126;}
+            else if (gateCode.value == "110") {
+                gateSelect.value = 973;}
+            else if (gateCode.value == "111") {
+                gateSelect.value = 974;}
+            else if (gateCode.value == "2") {
+                gateSelect.value = 127;}
+            else if (gateCode.value == "3") {
+                gateSelect.value = 128;}
+            else if (gateCode.value == "4") {
+                gateSelect.value = 129;}
+            else if (gateCode.value == "5") {
+                gateSelect.value = 130;}
+            else if (gateCode.value == "6") {
+                gateSelect.value = 131;}
+            else if (gateCode.value == "11") {
+                gateSelect.value = 133;}
+            else if (gateCode.value == "12") {
+                gateSelect.value = 132;}
+            else if (gateCode.value == "13") {
+                gateSelect.value = 134;}
+            else if (gateCode.value == "14") {
+                gateSelect.value = 135;}
+            else if (gateCode.value == "15") {
+                gateSelect.value = 136;}
+            else if (gateCode.value == "16") {
+                gateSelect.value = 137;}
+            else if (gateCode.value == "17") {
+                gateSelect.value = 138;}
+            else if (gateCode.value == "18") {
+                gateSelect.value = 139;}
+            else if (gateCode.value == "19") {
+                gateSelect.value = 140;}
+            else if (gateCode.value == "20") {
+                gateSelect.value = 141;}
+            else if (gateCode.value == "21") {
+                gateSelect.value = 142;}
+            else if (gateCode.value == "22") {
+                gateSelect.value = 143;}
+            else if (gateCode.value == "23") {
+                gateSelect.value = 144;}
+            else if (gateCode.value == "24") {
+                gateSelect.value = 145;}
+            else if (gateCode.value == "31") {
+                gateSelect.value = 149;}
+            else if (gateCode.value == "32") {
+                gateSelect.value = 150;}
+            else if (gateCode.value == "33") {
+                gateSelect.value = 151;}
+            else if (gateCode.value == "34") {
+                gateSelect.value = 152;}
+            else if (gateCode.value == "35") {
+                gateSelect.value = 153;}
+            else if (gateCode.value == "36") {
+                gateSelect.value = 154;}
+            else if (gateCode.value == "37") {
+                gateSelect.value = 155;}
+            else if (gateCode.value == "38") {
+                gateSelect.value = 156;}
+            else if (gateCode.value == "39") {
+                gateSelect.value = 157;}
+            else if (gateCode.value == "40") {
+                gateSelect.value = 158;}
+            else if (gateCode.value == "50") {
+                gateSelect.value = 332;}
+            else if (gateCode.value == "51") {
+                gateSelect.value = 333;}
+            else if (gateCode.value == "52") {
+                gateSelect.value = 334;}
+            else if (gateCode.value == "53") {
+                gateSelect.value = 335;}
+            else if (gateCode.value == "54") {
+                gateSelect.value = 336;}
+            else if (gateCode.value == "55") {
+                gateSelect.value = 337;}
+            else if (gateCode.value == "56") {
+                gateSelect.value = 338;}
+            else if (gateCode.value == "57") {
+                gateSelect.value = 339;}
+            else if (gateCode.value == "58") {
+                gateSelect.value = 340;}
+            else if (gateCode.value == "59") {
+                gateSelect.value = 341;}
+            else if (gateCode.value == "61") {
+                gateSelect.value = 342;}
+            else if (gateCode.value == "62") {
+                gateSelect.value = 343;}
+            else if (gateCode.value == "63") {
+                gateSelect.value = 344;}
+            else if (gateCode.value == "64") {
+                gateSelect.value = 345;}
+            else if (gateCode.value == "65") {
+                gateSelect.value = 346;}
+            else if (gateCode.value == "70") {
+                gateSelect.value = 347;}
+            else if (gateCode.value == "71") {
+                gateSelect.value = 348;}
+            else if (gateCode.value == "72") {
+                gateSelect.value = 349;}
+            else if (gateCode.value == "73") {
+                gateSelect.value = 350;}
+            else if (gateCode.value == "74") {
+                gateSelect.value = 355;}
+            else if (gateCode.value == "75") {
+                gateSelect.value = 356;}
+            else if (gateCode.value == "76") {
+                gateSelect.value = 357;}
+            else if (gateCode.value == "77") {
+                gateSelect.value = 358;}
+            else if (gateCode.value == "78") {
+                gateSelect.value = 359;}
+            else if (gateCode.value == "79") {
+                gateSelect.value = 360;}
+            else if (gateCode.value == "80") {
+                gateSelect.value = 361;}
+            else if (gateCode.value == "81") {
+                gateSelect.value = 362;}
+            else if (gateCode.value == "82") {
+                gateSelect.value = 377;}
+            else if (gateCode.value == "83") {
+                gateSelect.value = 378;}
+            else if (gateCode.value == "84") {
+                gateSelect.value = 365;}
+            else if (gateCode.value == "85") {
+                gateSelect.value = 366;}
+            else if (gateCode.value == "86") {
+                gateSelect.value = 381;}
+            else if (gateCode.value == "87") {
+                gateSelect.value = 382;}
+            else if (gateCode.value == "88") {
+                gateSelect.value = 383;}
+            else if (gateCode.value == "89") {
+                gateSelect.value = 384;}
+            else if (gateCode.value == "90") {
+                gateSelect.value = 385;}
+            else if (gateCode.value == "91") {
+                gateSelect.value = 386;}
+            else if (gateCode.value == "92") {
+                gateSelect.value = 387;}
+            else if (gateCode.value == "93") {
+                gateSelect.value = 388;}
+            else if (gateCode.value == "94") {
+                gateSelect.value = 389;}
+            else if (gateCode.value == "95") {
+                gateSelect.value = 390;}
+            else if (gateCode.value == "96") {
+                gateSelect.value = 391;}
+            else if (gateCode.value == "392") {
+                gateSelect.value = 97;}
+            else if(gateCode.value == "126") {
+                gateSelect.value = 12776;}
+            else if (gateCode.value == "127") {
+                gateSelect.value = 12776;
+            } else {
+                alert("Invalid Gate: " + gateCode.value);
+            }
+
+            // clear inputs
+            gateCode.value = "";
+            flightCode.value = "";
+            tailCode.value = "";
         }
-
-        // Gate Selector
-        var gateSelect = document.getElementById("Gate_Id");
-
-        if (gateCode.value == "A2" || gateCode.value == "a2") {
-            gateSelect.value = "12778";}
-        else if (gateCode.value == "A9" || gateCode.value == "a9") {
-            gateSelect.value = "12930";}
-        else if (gateCode.value == "1") {
-            gateSelect.value = 126;}
-        else if (gateCode.value == "110") {
-            gateSelect.value = 973;}
-        else if (gateCode.value == "111") {
-            gateSelect.value = 974;}
-        else if (gateCode.value == "2") {
-            gateSelect.value = 127;}
-        else if (gateCode.value == "3") {
-            gateSelect.value = 128;}
-        else if (gateCode.value == "4") {
-            gateSelect.value = 129;}
-        else if (gateCode.value == "5") {
-            gateSelect.value = 130;}
-        else if (gateCode.value == "6") {
-            gateSelect.value = 131;}
-        else if (gateCode.value == "11") {
-            gateSelect.value = 133;}
-        else if (gateCode.value == "12") {
-            gateSelect.value = 132;}
-        else if (gateCode.value == "13") {
-            gateSelect.value = 134;}
-        else if (gateCode.value == "14") {
-            gateSelect.value = 135;}
-        else if (gateCode.value == "15") {
-            gateSelect.value = 136;}
-        else if (gateCode.value == "16") {
-            gateSelect.value = 137;}
-        else if (gateCode.value == "17") {
-            gateSelect.value = 138;}
-        else if (gateCode.value == "18") {
-            gateSelect.value = 139;}
-        else if (gateCode.value == "19") {
-            gateSelect.value = 140;}
-        else if (gateCode.value == "20") {
-            gateSelect.value = 141;}
-        else if (gateCode.value == "21") {
-            gateSelect.value = 142;}
-        else if (gateCode.value == "22") {
-            gateSelect.value = 143;}
-        else if (gateCode.value == "23") {
-            gateSelect.value = 144;}
-        else if (gateCode.value == "24") {
-            gateSelect.value = 145;}
-        else if (gateCode.value == "31") {
-            gateSelect.value = 149;}
-        else if (gateCode.value == "32") {
-            gateSelect.value = 150;}
-        else if (gateCode.value == "33") {
-            gateSelect.value = 151;}
-        else if (gateCode.value == "34") {
-            gateSelect.value = 152;}
-        else if (gateCode.value == "35") {
-            gateSelect.value = 153;}
-        else if (gateCode.value == "36") {
-            gateSelect.value = 154;}
-        else if (gateCode.value == "37") {
-            gateSelect.value = 155;}
-        else if (gateCode.value == "38") {
-            gateSelect.value = 156;}
-        else if (gateCode.value == "39") {
-            gateSelect.value = 157;}
-        else if (gateCode.value == "40") {
-            gateSelect.value = 158;}
-        else if (gateCode.value == "50") {
-            gateSelect.value = 332;}
-        else if (gateCode.value == "51") {
-            gateSelect.value = 333;}
-        else if (gateCode.value == "52") {
-            gateSelect.value = 334;}
-        else if (gateCode.value == "53") {
-            gateSelect.value = 335;}
-        else if (gateCode.value == "54") {
-            gateSelect.value = 336;}
-        else if (gateCode.value == "55") {
-            gateSelect.value = 337;}
-        else if (gateCode.value == "56") {
-            gateSelect.value = 338;}
-        else if (gateCode.value == "57") {
-            gateSelect.value = 339;}
-        else if (gateCode.value == "58") {
-            gateSelect.value = 340;}
-        else if (gateCode.value == "59") {
-            gateSelect.value = 341;}
-        else if (gateCode.value == "61") {
-            gateSelect.value = 342;}
-        else if (gateCode.value == "62") {
-            gateSelect.value = 343;}
-        else if (gateCode.value == "63") {
-            gateSelect.value = 344;}
-        else if (gateCode.value == "64") {
-            gateSelect.value = 345;}
-        else if (gateCode.value == "65") {
-            gateSelect.value = 346;}
-        else if (gateCode.value == "70") {
-            gateSelect.value = 347;}
-        else if (gateCode.value == "71") {
-            gateSelect.value = 348;}
-        else if (gateCode.value == "72") {
-            gateSelect.value = 349;}
-        else if (gateCode.value == "73") {
-            gateSelect.value = 350;}
-        else if (gateCode.value == "74") {
-            gateSelect.value = 355;}
-        else if (gateCode.value == "75") {
-            gateSelect.value = 356;}
-        else if (gateCode.value == "76") {
-            gateSelect.value = 357;}
-        else if (gateCode.value == "77") {
-            gateSelect.value = 358;}
-        else if (gateCode.value == "78") {
-            gateSelect.value = 359;}
-        else if (gateCode.value == "79") {
-            gateSelect.value = 360;}
-        else if (gateCode.value == "80") {
-            gateSelect.value = 361;}
-        else if (gateCode.value == "81") {
-            gateSelect.value = 362;}
-        else if (gateCode.value == "82") {
-            gateSelect.value = 377;}
-        else if (gateCode.value == "83") {
-            gateSelect.value = 378;}
-        else if (gateCode.value == "84") {
-            gateSelect.value = 365;}
-        else if (gateCode.value == "85") {
-            gateSelect.value = 366;}
-        else if (gateCode.value == "86") {
-            gateSelect.value = 381;}
-        else if (gateCode.value == "87") {
-            gateSelect.value = 382;}
-        else if (gateCode.value == "88") {
-            gateSelect.value = 383;}
-        else if (gateCode.value == "89") {
-            gateSelect.value = 384;}
-        else if (gateCode.value == "90") {
-            gateSelect.value = 385;}
-        else if (gateCode.value == "91") {
-            gateSelect.value = 386;}
-        else if (gateCode.value == "92") {
-            gateSelect.value = 387;}
-        else if (gateCode.value == "93") {
-            gateSelect.value = 388;}
-        else if (gateCode.value == "94") {
-            gateSelect.value = 389;}
-        else if (gateCode.value == "95") {
-            gateSelect.value = 390;}
-        else if (gateCode.value == "96") {
-            gateSelect.value = 391;}
-        else if (gateCode.value == "392") {
-            gateSelect.value = 97;}
-        else if(gateCode.value == "126") {
-            gateSelect.value = 12776;}
-        else if (gateCode.value == "127") {
-            gateSelect.value = 12776;
-        } else {
-            alert("Invalid Gate: " + gateCode.value);
-        }
-
-        // clear inputs
-        gateCode.value = "";
-        flightCode.value = "";
-        tailCode.value = "";
     }
 
     function findFirstDescendant(parent, tagname) {
